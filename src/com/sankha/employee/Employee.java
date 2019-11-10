fa55474281fbe980ff6b1d8f9d54e7e929bc16d0
sankhadeep <sankhadeep2007@gmail.com>
2019-11-11 00:08:50 +0530

    First step

--- src/com/sankha/employee/Employee.java
+++ src/com/sankha/employee/Employee.java
@@ -1,5 +1,29 @@
 package com.sankha.employee;
 
 public class Employee {
-
+	String name;
+	int id;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
