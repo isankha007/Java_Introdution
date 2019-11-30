91f9e669214c88bc0d3295d6cc3729d0f8ff5a0b
sankhadeep <sankhadeep2007@gmail.com>
2019-12-01 02:23:21 +0530

    Doc added

--- src/com/sankha/exception/TestException.java
+++ src/com/sankha/exception/TestException.java
@@ -2,6 +2,7 @@ package com.sankha.exception;
 
 public class TestException {
 
+	static String s;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//unchecked exception 
@@ -10,10 +11,14 @@ public class TestException {
 		uc.killItNow(0); //java.lang.ArithmeticException:
 		uc.beyondBound(11);// java.lang.ArrayIndexOutOfBoundsException:
 		*/
+		/*
 		//checked Exception
 		CheckedException chk=new CheckedException();
 		chk.readItNow();
-		//chk.testCustomException();
+		chk.testCustomException();
+		*/
+		//System.out.println(s.length());//JVM exceptions
+		
 		
 
 	}
