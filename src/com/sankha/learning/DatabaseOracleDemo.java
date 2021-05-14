56fb4e79e7dcf763221ce1ed0595d4d7c258fd69
sankhadeep <sankhadeep2007@gmail.com>
2021-05-14 19:30:43 +0530

    Oracle connectivity addedusing Docker for mac

---
+++ src/com/sankha/learning/DatabaseOracleDemo.java
@@ -0,0 +1,35 @@
+package com.sankha.learning;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseOracleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			 
+			//step2 create  the connection object  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+			  
+			//step3 create the statement object  
+			Statement stmt=con.createStatement();  
+			  
+			//step4 execute query  
+			ResultSet rs=stmt.executeQuery("select * from sankha.Employee");  
+			while(rs.next())  
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			  
+			//step5 close the connection object  
+			con.close();  
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+
+	}
+
+}
